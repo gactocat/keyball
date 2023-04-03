38b8467a99391af220e1e0661a58c4fbd60f6765
gactocat <gactocat@gmail.com>
2023-04-03 19:35:08 +0900

    Set `TAPPING_FORCE_HOLD` 0

--- qmk_firmware/keyboards/keyball/keyball44/keymaps/cymric/config.h
+++ qmk_firmware/keyboards/keyball/keyball44/keymaps/cymric/config.h
@@ -20,6 +20,9 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 #define HOLD_ON_OTHER_KEY_PRESS
 
+#define TAPPING_FORCE_HOLD 
+//#define QUICK_TAP_TERM 0
+
 #pragma once
 
 #ifdef RGBLIGHT_ENABLE
